4e79ecd7f60ab3b39d147220d090735696014f6f
maulik14072002 <sojitramaulik2002@gmail.com>
2025-05-14 22:41:13 +0530

    header

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,8 +2,8 @@ import React, { memo } from 'react'
 
 export default memo(function Footer() {
   return (
-    <div>
-      footer
-    </div>
+    <>
+      
+    </>
   )
 })
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,30 @@
 import React, { memo } from 'react'
+import { Button, Container } from 'react-bootstrap'
+import logo from '../../assets/img/logo/sojitra-industries-2.png'
+import { Link } from 'react-router-dom'
 
 export default memo(function Header() {
   return (
     <>
-
-      
-       
+      <div className='header'>
+        <Container fluid="md">
+          <div className='logo py-2 d-flex align-items-center justify-content-between'>
+            <img src={logo} alt='sojitra-insutries' width="280px" />
+            <Button className='get-in-touch'>GET IN TOUCH</Button>
+          </div>
+        </Container>
+      </div>
+      <div className='menu'>
+        <Container fluid="md">
+          <ul>
+            <li><Link href='#'>Home</Link></li>
+            <li><Link href='#'>Company</Link></li>
+            <li><Link href='#'>Product</Link></li>
+            <li><Link href='#'>Quality</Link></li>
+            <li><Link href='#'>Contact</Link></li>
+          </ul>
+        </Container>
+      </div>
     </>
   )
 })
