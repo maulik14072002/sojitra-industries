c1a7e83eff32b63f2114adde3a9779e13ff6a6b9
maulik14072002 <sojitramaulik2002@gmail.com>
2025-06-08 21:29:40 +0530

    add about page

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 const About = () => {
   return (
     <Layout>
-      <h3>About</h3>
+      <section className='about_section'>
+        <Container fluid="md">
+          <Row>
+            <Col lg="12" md="12">
+              <div className='text-center'>
+                <h1>About</h1>
+                <p>Providing from Brass, S.S, Mild Steel, Copper & any special material as per customer requirements <br />Our target is to provide zero defects products  to client which is remarkable in industry.</p>
+                <Button className='contact-us-btn custom-btn mt-3'>Contact us</Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='business_section'>
+        <Container fluid="md">
+          <Row>
+            <Col lg="6" md="12">
+              <div className='business_info'>
+                <h2>We are the Leader in Industrial Business</h2>
+                <p>With decades of industry experience and an unwavering commitment to total quality, Sojitra Industries has earned a strong reputation in the global brass components market. Our precision-engineered products cater to a wide range of sectors, including automotive, electrical, defense, plastic molding, construction, and other industrial applications.</p>
+                <p className='mt-3'>From the beginning, our focus has been on continuous improvement—empowering our skilled workforce through ongoing training and investing in state-of-the-art CNC technology to drive innovation and efficiency. At Sojitra Industries, we strive to be a key player and a pioneer in the manufacturing and global supply of high-precision brass and metal-turned components.</p>
+                <div className='founder_name mt-5'>
+                  <h5>Mr. Maulik sojitra</h5>
+                  <p>Founder, Sojitra Industires</p>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6" md="12" className='mt-4 mt-mg-0'>
+              <div className='company_img'></div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
     </Layout>
   )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -20,8 +20,6 @@ export default memo(function Home() {
             <Col lg="12" md="12">
               <div className='text-start'>
                 <h1>Leading Manufacturer of Precision <br />Machined Components</h1>
-                {/* <p>Providing from Brass, S.S, Mild Steel, Copper & any special material as per customer requirements</p>
-              <p>Our target is to provide zero defects products  to client which is remarkable in industry. </p> */}
                 <p className='mt-2'>Our goal is maximum contribution to our customer’s growth  and to provide service to   their expectations.</p>
                 <Button className='contact-us-btn custom-btn mt-3'>Contact us</Button>
               </div>
