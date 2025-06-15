b752a095f29e8da1ea886fab3f835f5326401684
maulik14072002 <sojitramaulik2002@gmail.com>
2025-06-15 18:11:49 +0530

    add footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import { FaLinkedinIn } from 'react-icons/fa'
 import { ImFacebook } from 'react-icons/im'
 import { Link } from 'react-router-dom'
@@ -7,15 +7,49 @@ import { Link } from 'react-router-dom'
 export default memo(function Footer() {
   return (
     <div className='footer'>
-        <Container fluid="md">
-          <div className='logo_section d-flex align-items-center justify-content-between'>
-            <img src='/img/logo/sojitra-industries-2.png' alt='sojitra industires' width="280px"/>
-            <div className='on_social d-flex align-items-center gap-3'>
-              <Link href='' target="_blank"><ImFacebook /></Link>
-              <Link href='https://www.linkedin.com/company/sojitraindustry/' target="_blank"><FaLinkedinIn /></Link>
+      <Container fluid="md">
+        <Row>
+          <Col lg="4" md="6" sm="12">
+            <div className='footer_def'>
+              <img src='/img/logo/sojitra-industries-2.png' alt='sojitra industires' width="250px" />
+              <p>Sojitra Industries delivers precision-engineered components worldwide, uniting advanced manufacturing with uncompromising quality.</p>
+              <Button className='get-in-touch custom-btn mt-4' >Contact Us</Button>
             </div>
-          </div>
-        </Container>
+          </Col>
+          <Col lg="3" md="6" sm="12" className='mt-3 mt-md-0'>
+            <div className='footer_menu'>
+              <h5>Useful resources</h5>
+              <ul>
+                <li><Link to={{ pathname: "/" }}>Home</Link></li>
+                <li><Link to={{ pathname: "/about" }}>About</Link></li>
+                <li><Link to={{ pathname: "/product" }}>Product</Link></li>
+                <li><Link to={{ pathname: "/quality" }}>Quality</Link></li>
+                <li><Link to={{ pathname: "/contact" }}>Contact</Link></li>
+              </ul>
+            </div>
+          </Col>
+          <Col lg="2" md="6" sm="12" className='mt-3 mt-lg-0'>
+            <div className='footer_menu'>
+              <h5>On Social</h5>
+              <div className='on_social d-flex align-items-center gap-3 mt-3 mt-sm-4'>
+                <Link href='' target="_blank"><ImFacebook /></Link>
+                <Link href='https://www.linkedin.com/company/sojitraindustry/' target="_blank"><FaLinkedinIn /></Link>
+              </div>
+            </div>
+          </Col>
+          <Col lg="3" md="6" sm="12" className='mt-3 mt-lg-0'>
+            <div className='our_promise'>
+              <h5>Our Promise To You</h5>
+              <ul>
+                <li>24/7 customer support</li>
+                <li>Safe verified methods</li>
+                <li>Customized Products</li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+
+      </Container>
     </div>
   )
 })
