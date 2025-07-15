86f2cd579ebf9e353f2c84034d591f5b04dfa5b7
maulik14072002 <sojitramaulik2002@gmail.com>
2025-07-15 22:23:09 +0530

    add data

--- src/page/Product.jsx
+++ src/page/Product.jsx
@@ -5,14 +5,15 @@ import QualityControl from '../components/RepeatedComponents/QualityControl/Qual
 import IndustriesSlider from '../components/RepeatedComponents/SliderView/IndustriesSlider'
 import CustomerSupport from '../components/RepeatedComponents/CustomerSupport/CustomerSupport'
 import { useNavigate } from 'react-router-dom'
+import data from '../data/Product.json'
 
 const Product = () => {
 
-
     const navigate = useNavigate();
-    const goToNotFound = () => {
-        navigate('/404');
+    const goToNotFound = (item) => {
+        navigate('/404', {state : item});
     };
+    
 
     return (
         <Layout>
@@ -35,76 +36,19 @@ const Product = () => {
                     <div className='products_heading text-center'>
                         <h2>Types Of Products by Industry</h2>
                     </div>
-                    <Row>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card' onClick={goToNotFound}>
-                                <div className='product_img'><img alt='' src='/img/product/p1.webp' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'>
-                                    <h5>Brass Automotive Components</h5>
-                                    {/* <Button className='know_more'>Know more <FaArrowTrendUp /></Button> */}
-                                </div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p2.jpg' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>CNC Turned Components</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p4.jpg' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass Earthing Accessories</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p5.jpg' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass Electrical Components</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p6.jpg' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass Fittings</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p7.webp' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass Forged Parts</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p3.webp' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass Compression Parts</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p8.jpg' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass Inserts</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p9.webp' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass LPG & Gas Parts</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p10.jpg' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Brass Turned Components</h5></div>
-                            </div>
-                        </Col>
-                        <Col lg="4" md="6" sm="12" className='mt-4'>
-                            <div className='product_card'>
-                                <div className='product_img'><img alt='' src='/img/product/p11.jpg' width="100%" height="100%" /></div>
-                                <div className='px-3 py-2'><h5>Fasteners & Fixtures</h5></div>
-                            </div>
-                        </Col>
+                    <Row className='justify-content-center'>
+                        {
+                            data.map((i) =>
+                                <Col lg="4" md="6" sm="12" className='mt-4'>
+                                    <div className='product_card' onClick={() => goToNotFound(i)}>
+                                        <div className='product_img'><img alt='' src={i.img} width="100%" height="100%" /></div>
+                                        <div className='px-3 py-2'>
+                                            <h5>{i.name}</h5>
+                                        </div>
+                                    </div>
+                                </Col>
+                            )
+                        }
                     </Row>
                 </Container>
             </section>
